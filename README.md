08a55417b1a24742a554b1af91b83ea634c6405e
mynydd <paul.number12@googlemail.com>
2022-04-04 17:45:14 +0100

    out_dir cmd line arg

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # tei-solr
+
+## Start Solr
+
+```
+./bin/solr start -c -p 8983 -s example/cloud/node1/solr
+./bin/solr start -c -p 7574 -s example/cloud/node2/solr -z localhost:9983
+```
+
+Then go to [admin UI](http://localhost:8983/solr/#/)
+
+[Solr Tutorial](https://solr.apache.org/guide/8_11/solr-tutorial.html) is a good source of help.
+How to create a collection:
+```
+./bin/solr create -c lat-misc -s 2 -rf 2
+```
+
+How to post docs for indexing:
+```
+./bin/post -c lat-misc ~/Documents
+```
+
--- script.py
+++ script.py
@@ -1,14 +1,20 @@
 import argparse
+from pathlib import PurePath
 
-import lxml.etree as ET
+from lxml import etree
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("xml_filename", help="xml file name")
     parser.add_argument("xsl_filename", help="xsl file name")
+    parser.add_argument("out_dir", help="output directory")
     args = parser.parse_args()
-    dom = ET.parse(args.xml_filename)
-    xslt = ET.parse(args.xsl_filename)
-    transform = ET.XSLT(xslt)
+    out_file_path = PurePath(args.out_dir, PurePath(args.xml_filename).name)
+    print(out_file_path)
+    dom = etree.parse(args.xml_filename)
+    xslt = etree.parse(args.xsl_filename)
+    transform = etree.XSLT(xslt)
     newdom = transform(dom)
-    print(ET.tostring(newdom, pretty_print=True))
+    with open(out_file_path, "wb") as outfile:
+        newdom.write(outfile, encoding="utf-8", pretty_print=True,
+            xml_declaration=True)
